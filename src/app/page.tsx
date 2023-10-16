2caf30bb9ad3df949cc80acb72ef52bc026f2cc5
Alex <alex.akten@outlook.com>
2023-10-16 20:51:48 +1100

    eyes track mouse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
   const [theme, setTheme] = useState("light"); // Default theme is light
@@ -11,6 +11,53 @@ export default function Home() {
     } else {
       setTheme("light");
     }
+  };
+
+  const svgRef = useRef<SVGSVGElement | null>(null); // Added type for ref
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      // get mouse position
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+      moveIrisToMouse(mouseX, mouseY);
+    };
+
+    // Attach the event listener to the window
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      // Detach the event listener from the window
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const moveIrisToMouse = (mouseX: number, mouseY: number) => {
+    const leftIris = document.getElementById("leftIris");
+    const rightIris = document.getElementById("rightIris");
+
+    const moveIris = (
+      iris: any,
+      centerX: number,
+      centerY: number,
+      maxMove: number
+    ) => {
+      const dx = mouseX - centerX;
+      const dy = mouseY - centerY;
+
+      const distance = Math.min(maxMove, Math.sqrt(dx * dx + dy * dy));
+      const angle = Math.atan2(dy, dx);
+
+      const newX = centerX + distance * Math.cos(angle);
+      const newY = centerY + distance * Math.sin(angle);
+
+      iris.setAttribute("cx", newX);
+      iris.setAttribute("cy", newY);
+    };
+
+    moveIris(leftIris, 7.86985, 14.0945, 4.97632); // (iris, centerX, centerY, max movement)
+    moveIris(rightIris, 36.3484, 7.86875, 4.97632);
   };
 
   return (
@@ -24,6 +71,7 @@ export default function Home() {
     >
       <nav className="flex justify-between relative">
         <svg
+          ref={svgRef}
           width="45"
           height="63"
           viewBox="0 0 45 63"
@@ -38,17 +86,32 @@ export default function Home() {
             d="M35.8738 40.5294L36.8704 45.6216L18.9579 49.1276L20.9688 59.4018L16.5955 60.2578L13.5879 44.8913L35.8738 40.5294Z"
             fill={theme === "light" ? "black" : "white"}
           />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M7.86863 21.9631C12.2144 21.9631 15.7373 18.4402 15.7373 14.0945C15.7373 9.74874 12.2144 6.22583 7.86863 6.22583C3.52291 6.22583 0 9.74874 0 14.0945C0 18.4402 3.52291 21.9631 7.86863 21.9631ZM7.86947 16.9868C9.46685 16.9868 10.7618 15.6919 10.7618 14.0945C10.7618 12.4971 9.46685 11.2022 7.86947 11.2022C6.27209 11.2022 4.97716 12.4971 4.97716 14.0945C4.97716 15.6919 6.27209 16.9868 7.86947 16.9868Z"
+
+          <circle
+            cx="7.86863"
+            cy="14.0945"
+            r="7.86863"
             fill={theme === "light" ? "black" : "white"}
           />
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M36.2026 15.7373C40.5483 15.7373 44.0712 12.2144 44.0712 7.86863C44.0712 3.52291 40.5483 0 36.2026 0C31.8569 0 28.334 3.52291 28.334 7.86863C28.334 12.2144 31.8569 15.7373 36.2026 15.7373ZM36.348 10.7611C37.9454 10.7611 39.2403 9.46616 39.2403 7.86878C39.2403 6.2714 37.9454 4.97647 36.348 4.97647C34.7506 4.97647 33.4557 6.2714 33.4557 7.86878C33.4557 9.46616 34.7506 10.7611 36.348 10.7611Z"
+          <circle
+            id="leftIris"
+            cx="7.86985"
+            cy="14.0945"
+            r="2.89231"
+            fill={theme === "light" ? "white" : "black"}
+          />
+          <circle
+            cx="36.2026"
+            cy="7.86863"
+            r="7.86863"
             fill={theme === "light" ? "black" : "white"}
+          />
+          <circle
+            id="rightIris"
+            cx="36.3484"
+            cy="7.86875"
+            r="2.89231"
+            fill={theme === "light" ? "white" : "black"}
           />
         </svg>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
