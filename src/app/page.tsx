b6c0bf1cd4c56becd2c32cccc3619aeeceef4f06
Alex <alex.akten@outlook.com>
2024-01-04 12:21:13 +0100

    kindred link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
               <li>/ projects</li>
               <li>
                 <a
-                  href="https://kindredlab.vercel.app/"
+                  href="https://www.kindredlab.io/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
