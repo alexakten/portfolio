1d2067b9e52e84d9fefdd2978821a1aa820d49d6
Alex <alex.akten@outlook.com>
2024-01-16 15:59:27 +0100

    post

--- src/app/blogPosts.tsx
+++ src/app/blogPosts.tsx
@@ -8,6 +8,13 @@
 
 const blogPosts = [
   {
+  title: "the pull of the arrival fallacy.",
+  date: "Jan 7, 2024",
+  content: `
+  `,
+  slug: "point-of-building",
+},
+  {
     title: "making medicine, not vitamins.",
     date: "Jan 7, 2024",
     content: `Creating solutions for problems that don't even exist is tempting. That's how most of my ideas come about. "Wouldn't it be fun if there was an app that did [fill in the blank]?". This thought strikes me probably ten times a day. But I have to remind myself to resist the temptation to dive deeper into the thought. Because most likely, the idea is pretty shit and no one would be interested in paying for an app that did [fill in the blank]. 
@@ -23,8 +30,9 @@ const blogPosts = [
 
     But now, I might've find something that I'm truly excited to build. And no, it's not another project manager. It's not a shitty to-do list app. It's a problem that is difficult enough that I yet have no idea how to really solve.  And that is exciting to me. It's not just copying someone else's idea either, like I've done with many of my previous projects. It's a space where I truly have to be innovative and learn a lot before I know what to even build. But just contributing to this space somehow is something that really motivates me.
     
-    The space is climate tech, and helping reduce deforestation in Brazil. I want to contribute somehow to making not cutting down trees more profitable than using the land for agriculture, which is a big reason for deforestation. If successful, there is a possibility to create a win-win-win situation for landowners, investors (companies emitting gases wanting to offset their emissions), and the planet. This is just an idea for now, but I have to start somewhere.
+    The space is climate tech, and helping farmers in Brazil. I want to contribute somehow to helping farmers implement sustainable farming practices that generate carbon credits. If successful, there is a possibility to create a win-win-win situation for farmers, investors (companies emitting gases wanting to offset their emissions), and the planet. This is just an idea for now, but I have to start somewhere.
 
+    There are so many challenges that need to be solved in order for a system like carbon credits to work. Measuring what the effects are, verifying them, and then keeping carbon from escaping back into the atmosphere, there are so many places where I could contribute.
 
     `,
 
