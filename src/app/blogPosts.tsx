bb634dc2b4eb975ab6662e2a1cc64f2f3bdab6a9
Alex <alex.akten@outlook.com>
2024-01-25 23:54:58 +0100

    good day blog post

--- src/app/blogPosts.tsx
+++ src/app/blogPosts.tsx
@@ -1,6 +1,6 @@
 // {
 //   title: "title",
-//   date: "Jan 1, 2000",
+//   date: "Jan 1, 2024",
 //   content: `
 //   `,
 //   slug: "first",
@@ -8,7 +8,22 @@
 
 const blogPosts = [
   {
-    title: "making medicine, not vitamins.",
+    title: "having a good day.",
+    date: "Jan 25, 2024",
+    content: `Today wasn't really a good day. Not that anything bad happened. It just wasn't an ideal day. I thought about why. And I came to the conclusion that I didn't fill my buckets. I have this idea of three buckets that need to be filled in order for me to have an optimal day. It's really simple:
+
+    1. The Productivity Bucket. I need to feel that I've done something challenging and overcome some type of problem that is hard enough that it takes some actual effort. It can't be something that someone else has told me to do. In order for this bucket to be filled, it must be something that I myself have set as an obstacle for myself.
+
+    2. The Social Bucket. I need to see people. Preferably, I need to talk to people. Today I saw a lot of people but I didn't say a single word to anyone. I enjoy solitude, but I've realized that I really need to talk to people face-to-face for a bit every day. I'm happy that I at least got out of my apartment. I can't stay at home the whole day. That's a rule I cannot break, ever.
+
+    3. The Exercise Bucket. Pretty self explanatory. Climbing makes a day automatically good. Same with going for a run. On days that I do neither I feel pretty bad. It's so simple, but this is one that I procrastinate on a lot. Even going for a walk helps with my mood.
+
+    I wrote all of this as a reminder to myself of how easy it is to have a good day. I just need to fill the buckets, and I know I will feel good. Yet I have plenty of bad days because I don't make the effort to plan my days around the three buckets. From now on, I'll try to keep them more in mind.
+  `,
+    slug: "a-good-day",
+  },
+  {
+    title: "making medicine & not vitamins.",
     date: "Jan 7, 2024",
     content: `Creating solutions for problems that don't even exist is tempting. That's how most of my ideas come about. "Wouldn't it be fun if there was an app that did [fill in the blank]?". This thought strikes me probably ten times a day. But I have to remind myself to resist the temptation to dive deeper into the thought. Because most likely, the idea is pretty shit and no one would be interested in paying for an app that did [fill in the blank]. 
     
@@ -34,3 +49,13 @@ const blogPosts = [
 ];
 
 export default blogPosts;
+
+// title: "what is the end goal?",
+// date: "Jan 16, 2024",
+// content: `If money were no object, what would I spend my days doing? I think about this question a lot. My goal is to get to a point where my answer is no different from my reality. What does that mean for me? Right now, there is nothing that I could buy that would make me any happier than I am, which means money is not what I desire. Is that really true? I think so. Would traveling make me happier? Probably for a while, but then I would get understimulated. Would eating at a bunch of fancy restaurants make me happier? In the beginning, yeah, but then I would get used to it.
+
+// Then what is it that I want? Well, if I go back to my first question, the first change I'd make to my life is that I would probably not be doing any agency work. Trading my time for money is not what I want in the long term. I have this idea of wanting to work at my full potential, and doing agency work just doesn't do that for me. It's a fun side project, but the problem is that it just doesn't require me working at my maximum capacity, at the edge of my abilities.
+
+// What learning to code has taught me is that there is nothing more satisfying than tackling something I have no idea how to approach at first, and learning just enough to make some progress towards achieving it. Knowing myself though, it just can't be too hard or I struggle maintaining motivation to keep doing it. So, I guess the ideal for me is to work on problems that are really challenging in many ways but not impossible.
+// `,
+// slug: "the-end-goal",
