f94791dedcee5eeab47d0d0ff845b50847da7508
Alex <alex.akten@outlook.com>
2024-04-30 10:29:37 +0200

    new tab

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -10,6 +10,8 @@ export default function Project({
   return (
     <a
       href={link}
+      target="_blank"
+      rel="noopener noreferrer"
       className="-mx-3 mt-3 flex w-full flex-col rounded-md p-3 px-3 font-normal no-underline  sm:hover:bg-zinc-900 sm:py-3"
     >
       <p className="">{name}</p>
