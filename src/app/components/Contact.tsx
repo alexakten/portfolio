77415ecdd4811b010610e427afd86b3450a67aee
Alex <alex.akten@outlook.com>
2024-04-30 10:23:54 +0200

    no hover on mobile

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -10,7 +10,7 @@ export default function Contact({
   return (
     <a
       href={link}
-      className="-mx-3 mt-3 flex w-full flex-col rounded-md p-3 px-3 font-normal no-underline hover:bg-zinc-900 sm:py-3"
+      className="-mx-3 mt-3 flex w-full flex-col rounded-md p-3 px-3 font-normal no-underline sm:hover:bg-zinc-900 sm:py-3"
     >
       <p className="">{name}</p>
       <p className="opacity-80 text-sm">{description}</p>
